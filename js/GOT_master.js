3f71a27d54e894b47c73c3e7b0e18503b5a62a45
Cunha <r_cunha@tss-hr420-dm18.fc.ca>
2019-03-18 13:12:41 -0400

    updating files for homework

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -2,19 +2,47 @@
 	// stub
 	console.log('fired');
 
-	const 	sigils 		= document.querySelectorAll('.sigil-container'),
-			lightbox 	= document.querySelector('.lightbox'),
-			video		= document.querySelector('video');
+	const 	sigils		= document.querySelectorAll(".sigil-container"),
+			lightbox 	= document.querySelector('.lightBox'),
+			video		= document.querySelector('video'),
+			lbClose     = document.querySelector('.lightbox-close'),
+			topBanners  = document.querySelector('#houseImages');
 
 	function openLightbox() {
+		//debugger;
+		let targetHouse = this.className.split(" ")[1];
+
+		// this gives us a lowercase house name -> the second class on all of the shields ie stark, baratheon
+		
+
+		// flip this uppercase
+		let targetVid = targetHouse.charAt(0).toUpperCase() + targetHouse.slice(1);
+
+		video.src = `video/House-${targetVid}.mp4`;
 		lightbox.classList.add('lightbox-on');
+
+		video.load();
+		video.play();
 	}
 	
 	function closeLightbox() {
-		lightbox.classList.remove('lightbox-on');
+		lightBox.classList.remove('lightbox-on');
+	
+	// rewind the current video and pause it
+		video.currentTime = 0;
+		video.pause();
 	}
 
-	sigils.forEach(sigil => sigil.addEventListener('click', openLightbox));
+	function animateBanners() {
+		const offSet = 600;
+		let currentOffset = this.dataset.offset * offSet;
+
+		topBanners.style.right = currentOffset + "px";
+
+	};
+
+	sigils.forEach(sigil => sigil.addEventListener('click', animateBanners));
 
 	video.addEventListener('ended', closeLightbox);
+	lbClose.addEventListener('click', closeLightbox);
 })();
